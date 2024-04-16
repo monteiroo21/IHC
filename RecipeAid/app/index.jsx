abb97994d4bdae45a644d41a6f4ee44539720f22
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-04-16 15:24:01 +0100

    change folder

---
+++ RecipeAid/app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">RecipeAid!</Text>
+            <StatusBar style="auto" />
+            <Link href="/profile" style={{ color: 'blue' }}>Got to Profile!</Link>
+        </View>
+    );
+}
