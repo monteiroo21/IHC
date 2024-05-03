4b606b045cabfa590c48e32b700ddf41ea7a148e
Joao <joaomonteiro29@ua.pt>
2024-05-03 16:10:56 +0100

    card

--- RecipeAid/components/Card.jsx
+++ RecipeAid/components/Card.jsx
@@ -7,18 +7,18 @@ const Card = ({ title, user, image, rating }) => {
     return (
         <View className="flex-col items-center px-4">
             <Image source={image} className="w-72 h-56 rounded-xl relative" />
-            <View className="absolute mt-36 left-10">
-                <Text className="text-white text-xl font-bold w-72">
+            <View style={{ backgroundColor: 'rgba(71, 85, 105, 0.65)' }} className="absolute mt-36 rounded-b-xl bg-slate-500 top-1">
+                <Text className="text-white left-5 text-xl font-bold w-72">
                     {title}
                 </Text>
-                <Text className="text-white text-sm font-bold">
+                <Text className="text-white left-5 text-sm font-bold">
                     {user}
                 </Text>
-                <View className="z-0">
-                    <Text className="text-white text-sm font-bold">
+                <View className="flex-row left-5">
+                    <Text className="text-white text-lg font-bold pr-2">
                         {rating}
-                        <Stars value={rating} className="z-0 start-2" />
                     </Text>
+                    <Stars value={rating} />
                 </View>
             </View>
         </View>
