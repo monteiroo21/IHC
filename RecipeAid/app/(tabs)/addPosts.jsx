800b30ba48079d5a6d9bbac3f361955a1bf26b80
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-05-11 12:17:33 +0100

    dropdows

--- RecipeAid/app/(tabs)/addPosts.jsx
+++ RecipeAid/app/(tabs)/addPosts.jsx
@@ -5,8 +5,10 @@ import { icons } from '../../constants/icons'
 import CardStandart from '../../components/CardStandart'
 import { myRecipes, recipes } from '../../constants/data';
 import * as ImagePicker from 'expo-image-picker';
-import { StatusBar } from 'expo-status-bar'
-import SearchBar from '../../components/SearchBar'
+import { StatusBar } from 'expo-status-bar';
+import { SelectList } from 'react-native-dropdown-select-list'
+import { icon } from '@fortawesome/fontawesome-svg-core'
+
 
 const addButton = ({ icon, color, name, buttonStyle }) => {
     return (
@@ -43,6 +45,23 @@ const addPosts = () => {
     const myLastRecipes = myRecipes.slice(-2);
     const [modalVisible, setModalVisible] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedTypeOfMeals, setSelectedTypeOfMeals] = useState("");
+    const [selectedTypeOfCuisine, setSelectedTypeOfCuisine] = useState("");
+
+    const typeOfMeals = [
+        { key: 'breakfast', value: 'Breakfast' },
+        { key: 'lunch', value: 'Lunch' },
+        { key: 'dinner', value: 'Dinner' },
+        { key: 'snack', value: 'Snack' },
+    ];
+
+    const typeOfCuisine = [
+        { key: 'Italian', value: 'Italian' },
+        { key: 'Mexican', value: 'Mexican' },
+        { key: 'Chinese', value: 'Chinese' },
+        { key: 'Japanese', value: 'Japanese' },
+    ];
+
 
     const [formRecipe, setFormRecipe] = useState({
         user: "Manuel Augusto",
@@ -110,6 +129,7 @@ const addPosts = () => {
                                     </View>
                                 </TouchableOpacity>
                             </View>
+
                             <View className="mt-6 justify-center items-center">
                                 <Text className="text-2xl font-extrabold ml-2 text-teal-700 mb-2">Name of recipe</Text>
                                 <View className='flex flex-row items-center space-x-4 w-80 h-12 px-4 bg-black-100 rounded-2xl border-4 border-gray-400 focus:border-secondary'>
@@ -119,6 +139,41 @@ const addPosts = () => {
                                         placeholder='Recipe name ...'
                                         placeholderTextColor="#000"
                                         onChangeText={(e) => setFormRecipe({ ...formRecipe, title: e })}
+                                    />
+                                </View>
+                            </View>
+
+                            <View className="mt-6 flex-row justify-between px-4">
+                                <View className="flex flex-col items-center">
+                                    <Text className="text-xl font-bold ml-2 text-teal-700 mb-2">Type of meal</Text>
+                                    <SelectList
+                                        setSelected={setSelectedTypeOfMeals}
+                                        data={typeOfMeals}
+                                        placeholder='Select ...'
+                                        boxStyles={{ backgroundColor: '#fff', borderRadius: 16, borderColor: '#9ca3af', borderWidth: 4, width: 150, height: 50 }}
+                                        dropdownTextStyles={{ color: '#333', fontSize: 17 }}
+                                        placeholderStyles={{ color: '#888', fontSize: 18, fontWeight: 'extrabold' }}
+                                        dropdownStyles={{ backgroundColor: '#f9f9f9', borderColor: '#9ca3af', borderWidth: 4 }}
+                                        searchicon={<Image source={icons.search} resizeMode='contain' className="w-4 h-4" />}
+                                        searchPlaceholder=''
+                                        inputStyles={{ color: '#333', fontSize: 15, fontWeight: 'bold' }}
+
+                                    />
+                                </View>
+
+                                <View className="flex flex-col items-center">
+                                    <Text className="text-xl font-bold ml-2 text-teal-700 mb-2">Type of cuisine</Text>
+                                    <SelectList
+                                        setSelected={setSelectedTypeOfCuisine}
+                                        data={typeOfCuisine}
+                                        placeholder='Select ...'
+                                        boxStyles={{ backgroundColor: '#fff', borderRadius: 16, borderColor: '#9ca3af', borderWidth: 4, width: 150, height: 50 }}
+                                        dropdownTextStyles={{ color: '#333', fontSize: 17 }}
+                                        placeholderStyles={{ color: '#888', fontSize: 18, fontWeight: 'extrabold' }}
+                                        dropdownStyles={{ backgroundColor: '#f9f9f9', borderColor: '#9ca3af', borderWidth: 4 }}
+                                        searchicon={<Image source={icons.search} resizeMode='contain' className="w-4 h-4" />}
+                                        searchPlaceholder=''
+                                        inputStyles={{ color: '#333', fontSize: 15, fontWeight: 'bold' }}
                                     />
                                 </View>
                             </View>
